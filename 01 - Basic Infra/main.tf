010cd3987bc91628667b799ab33bf4f66f9ba945
Sanjay-George <sanjaygeorge16@gmail.com>
2022-10-27 14:46:10 +0200

    Add Cloud NAT and Router for private CI network
    Signed-off-by: Sanjay-George <sanjaygeorge16@gmail.com>

--- 01 - Basic Infra/main.tf
+++ 01 - Basic Infra/main.tf
@@ -35,6 +35,29 @@ resource "google_compute_subnetwork" "webserver_subnet" {
   private_ip_google_access = true
 }
 
+# CLOUD ROUTER AND NAT GATEWAY FOR CI NETWORK
+resource "google_compute_router" "ci_router" {
+  name    = "ci-router"
+  region  = google_compute_subnetwork.ci_subnet.region
+  network = google_compute_network.vpc.id
+
+  bgp {
+    asn = 64514
+  }
+}
+resource "google_compute_router_nat" "nat" {
+  name                               = "ci-router-nat"
+  router                             = google_compute_router.ci_router.name
+  region                             = google_compute_router.ci_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
 # FIREWALL RULES
 resource "google_compute_firewall" "all_egress" {
   name    = "all-egress"
